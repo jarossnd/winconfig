523a0389fc7f086a82ff2b6320e52639d1236898
Jason Ross <72239230+jarossnd@users.noreply.github.com>
2024-05-20 17:33:33 -0500

    Update 1-install_dependencies.ps1

--- 1-install_dependencies.ps1
+++ 1-install_dependencies.ps1
@@ -1,11 +1,15 @@
-git clone https://github.com/wbthomason/packer.nvim "C:\Users\%USERNAME%\AppData\Local\nvim-data\site\pack\packer\start\packer.nvim"
+winget install --id Git.Git -e --source winget
 
-winget install -e --id zig.zig
+winget install --id GitHub.cli
 
 winget install Neovim.Neovim
 
-winget install python.python.3.12
+winget install -e --id zig.zig
 
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
+winget install python.python.3.12
+
+git clone https://github.com/wbthomason/packer.nvim "C:\Users\%USERNAME%\AppData\Local\nvim-data\site\pack\packer\start\packer.nvim"
 
 pip install pynvim

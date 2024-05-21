05b00a1c1bc56d85d8aa5d7a21eb460eac307c76
Jason Ross <72239230+jarossnd@users.noreply.github.com>
2024-05-20 21:54:13 -0500

    Update 1-install_dependencies.ps1

--- 1-install_dependencies.ps1
+++ 1-install_dependencies.ps1
@@ -10,6 +10,6 @@ $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";"
 
 winget install python.python.3.12
 
-git clone https://github.com/wbthomason/packer.nvim %LOCALAPPDATA%\nvim-data\site\pack\packer\start\packer.nvim
+git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
 
 pip install pynvim

e7fa993463e4bac4787e5d5bfcd85a52ccd58d3b
Jason Ross <jaross@microsoft.com>
2024-05-23 18:25:02 -0500

    updates

--- nvim/lua/jason/remap.lua
+++ nvim/lua/jason/remap.lua
@@ -1,4 +1,11 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Exit on jj and jk
+vim.keymap.set('n', 'j', 'gj')
+vim.keymap.set('n', 'k', 'gk')
+
+-- Exit on jj and jk
+vim.keymap.set('i', 'jj', '<ESC>')
+vim.keymap.set('i', 'jk', '<ESC>')
 
